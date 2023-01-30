62f27edaab4ba90b55dfbba92defaea727cc993d
huyFPTthainguyen <15042001huy@gmail.com>
2023-01-30 13:23:26 +0700

    Tag of current_user

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -4,13 +4,13 @@ class ExamsController < ApplicationController
 
     def newest
         @newests = Exam.all.reorder(created_at: :desc).paginate(page: params[:page], per_page: 3)
-        user_exam = Exam.joins(:user_exams).where('user_exams.user_id')
+        user_exam = Exam.joins(:user_exams).where('user_exams.user_id = ?', current_user.id)
         @most_relates = user_exam.joins(:exam_tags).where('exam_tags.tag_id').reorder(created_at: :desc).distinct.paginate(page: params[:page], per_page: 3)
     end
 
     def completed
         @completes = Exam.all.reorder(finished_at: :desc).paginate(page: params[:page], per_page: 3)
-        user_exam = Exam.joins(:user_exams).where('user_exams.user_id')
+        user_exam = Exam.joins(:user_exams).where('user_exams.user_id = ?', current_user.id)
         @most_relates = user_exam.joins(:exam_tags).where('exam_tags.tag_id').reorder(created_at: :desc).distinct.paginate(page: params[:page], per_page: 3)
     end
 end
