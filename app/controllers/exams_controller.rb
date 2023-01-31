b281379d8d40dcaf675a3adc0302e31f352ddd95
huyFPTthainguyen <15042001huy@gmail.com>
2023-01-31 14:03:49 +0700

    profile exam

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -2,6 +2,11 @@ class ExamsController < ApplicationController
     def index
     end
 
+    def show
+        @exam = Exam.find(params[:id])
+        @tag_exams = @exam.tags
+    end
+
     def newest
         @newests = Exam.all.reorder(created_at: :desc).paginate(page: params[:page], per_page: 3)
         user_exam = Exam.joins(:user_exams).where('user_exams.user_id = ?', current_user.id)
